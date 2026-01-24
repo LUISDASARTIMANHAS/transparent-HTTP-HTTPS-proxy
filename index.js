050cc5439293995f5b938f1432de60aa9d119043
HELLOKITTY-I5-L <luisaugustodesouza785@gmail.com>
2026-01-24 17:05:39 -0300

    Adiciona função para iniciar proxy TCP para Minecraft com logging de tráfego

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import { startMinecraftProxy } from "./src/proxy/tcpProxy.js";
 import { startServer } from "./src/server.js";
 
-startServer(8080);
+// startServer(8080);
+/**
+ * Inicializa o proxy Minecraft
+ */
+startMinecraftProxy({
+  listenPort: 25565,
+  targetHost: "mc.skyzermc.com.br",
+  targetPort: 25652
+});
---
+++ src/proxy/tcpProxy.js
@@ -0,0 +1,39 @@
+import net from "net";
+import { logTraffic } from "../utils/trafficLogger.js";
+
+/**
+ * Inicia um proxy TCP para Minecraft
+ * @param {Object} options
+ * @param {number} options.listenPort Porta pública
+ * @param {string} options.targetHost Servidor real
+ * @param {number} options.targetPort Porta real
+ * @return {void}
+ */
+export function startMinecraftProxy({ listenPort, targetHost, targetPort }) {
+  const server = net.createServer((clientSocket) => {
+    const serverSocket = net.connect(targetPort, targetHost);
+
+    logTraffic({
+      protocol: "TCP",
+      clientIp: clientSocket.remoteAddress,
+      targetHost,
+      targetPort,
+      note: "Nova conexão minecraft",
+    });
+    serverSocket.write("RNP Proxy\n");
+
+    clientSocket.pipe(serverSocket);
+    serverSocket.pipe(clientSocket);
+
+    clientSocket.on("error", () => serverSocket.destroy());
+    serverSocket.on("error", () => clientSocket.destroy());
+  });
+
+	server.on("end", () => {
+      console.log("client disconnected");
+    });
+
+  server.listen(listenPort, () => {
+    console.log(`Proxy Minecraft ativo na porta ${listenPort}`);
+  });
+}
